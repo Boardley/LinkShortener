78ab65dd0d5c6f4fca4d02f8cae7f937644442b3
mj305 <maria.j.beckles@gmail.com>
2020-08-28 14:46:45 -0400

    added state on empty fields and copy icon

--- src/components/FetchComponent/FetchComponent.jsx
+++ src/components/FetchComponent/FetchComponent.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+localStorage.setItem("links", JSON.stringify([]));
+/* const a = [];
+a.push(JSON.parse(localStorage.getItem("links")));
+ */
+const SaveDataToLocalStorage = (data) => {
+  let a = [];
+  // Parse the serialized data back into an aray of objects
+  a = JSON.parse(localStorage.getItem("links")) || [];
+  // Push the new data (whether it be an object or anything else) onto the array
+  a.push(data);
+  // Alert the array value
+  alert(a); // Should be something like [Object array]
+  // Re-serialize the array back into a string and store it in localStorage
+  localStorage.setItem("links", JSON.stringify(a));
+};
 
 const FetchComponent = () => {
-  const [query, setQuery] = useState("");
   const [link, setLink] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  const { register, errors } = useForm();
+  const { register, errors, handleSubmit } = useForm();
 
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -14,20 +31,22 @@ const FetchComponent = () => {
     "__cfduid=d9160954faa4bd270b91e63679b6040f81598406033"
   );
 
-  const raw = JSON.stringify({
-    url: `${query}`,
-  });
-
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-
   const CORSURL = "https://cors-anywhere.herokuapp.com/";
 
-  const fetchRequest = () => {
+  const onSubmit = (data) => {
+    setLoading(true);
+
+    const raw = JSON.stringify({
+      url: `${data.linkShortener}`,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
     fetch(CORSURL + `https://rel.ink/api/links/`, requestOptions)
       .then((response) => response.json())
       .then((result) => {
@@ -36,35 +55,38 @@ const FetchComponent = () => {
           shortenUrl: `https://rel.ink/${result.hashid}`,
         };
         setLink(newUrl);
+        setLoading(false);
+        SaveDataToLocalStorage(newUrl);
       })
-      .catch((error) => console.log("Error: ", error));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchRequest();
-  };
-
-  const handleChange = (e) => {
-    setQuery(e.target.value);
+      .catch((error) => {
+        console.log("Error: ", error);
+        setLoading(false);
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label>Shorten Your Link *</label>
 
       <input
-        onChange={handleChange}
         name="linkShortener"
         placeholder="Enter Link Here..."
         type="text"
         ref={register({ required: true })}
       />
 
-      {errors.linkShortener && <span>Link is required</span>}
+      <button type="submit"> Shorten Link </button>
+      {loading && <p>Shortening Your Long Link...</p>}
 
-      <button type="submit">Shorten It</button>
+      {errors.linkShortener && <p>Link is required</p>}
+
       <p>{link.shortenUrl}</p>
+
+      {link.shortenUrl && (
+        <CopyToClipboard text={link.shortenUrl}>
+          <i className="fas fa-copy"></i>
+        </CopyToClipboard>
+      )}
     </form>
   );
 };
